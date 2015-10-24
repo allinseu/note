ea649d11ede809d14ec94f946dc2d58bcb56eba9
star <873111137@qq.com>
2015-10-24 10:43:54 +0800

    bug fix

--- js/util.js
+++ js/util.js
@@ -1,101 +1,108 @@
-(function(window,document){
-	var util = {};
+(function (window, document) {
+    var util = {};
 
-	// 检查notebook，如果为空隐藏删除按钮，并且为所有笔记本绑定点击时间
-	util.checkNotebook = function(clickNotebook){
-		if($('#notebooks').children('li').length === 0){
-			$('.deleteNotebook').hide();
-		}else{
-			$('.notebook').find('a').unbind();
-			$('.deleteNotebook').show();
-			//$('.notebook').find('a').click(clickNotebook);
-			$(document).on('click','.notebook',clickNotebook);
-		}
+    // 检查notebook，如果为空隐藏删除按钮，并且为所有笔记本绑定点击时间
+    util.checkNotebook = function (clickNotebook) {
+        if ($('#notebooks').children('li').length === 0) {
+            $('.deleteNotebook').hide();
+        } else {
+            $('.notebook').find('a').unbind();
+            $('.deleteNotebook').show();
+            //$('.notebook').find('a').click(clickNotebook);
 
-	}
+            // 绑定事件之前首先清理事件，否则事件会触发多次
+            $('.notebook').unbind();
+            $('.notebook').bind('click', clickNotebook);
+        }
 
-	util.checkCatalogue = function(clickCatalogue){
-		$(document).on('click', '.catalogue', clickCatalogue);
-	}
+    }
 
-	util.setTimer = function(fn,interval){
-		var recurse, ref;
-		ref = {};
-		ref.continue = true;
-		(recurse = function(){
-			if(ref.continue){
-				ref.timeout = setTimeout(function(){
-					fn();
-					recurse();
-				},interval);
-			}
-		})();
-		return ref;
-	};
+    util.checkCatalogue = function (clickCatalogue) {
+        // console.log('check Catalogue');
 
-	util.clearTimer = function(ref){
-		ref['continue'] = false;
-		clearTimeout(ref);
-	};
+        $('.catalogue').unbind();
+        $('.catalogue').bind('click', clickCatalogue);
+        // $(document).on('click', '.catalogue', clickCatalogue);
+    }
 
-	util.DataBinder = function(object_id){
-		// 使用一个jQuery对象作为简单的订阅者发布者
-		var pubSub = $({});
+    util.setTimer = function (fn, interval) {
+        var recurse, ref;
+        ref = {};
+        ref.continue = true;
+        (recurse = function () {
+            if (ref.continue) {
+                ref.timeout = setTimeout(function () {
+                    fn();
+                    recurse();
+                }, interval);
+            }
+        })();
+        return ref;
+    };
 
-		var data_attr = "bind-"+object_id,
-			message   = object_id+":change";
+    util.clearTimer = function (ref) {
+        ref['continue'] = false;
+        clearTimeout(ref);
+    };
 
-		$(document).on('change','[data-'+object_id+']',function(event){
-			var $input = $(this);
-			pubSub.trigger(message, [$input.data(data_attr), $input.val()]);
-		});
+    util.DataBinder = function (object_id) {
+        // 使用一个jQuery对象作为简单的订阅者发布者
+        var pubSub = $({});
 
-		pubSub.on('message', function(event,prop_name,new_val){
-			$('[data-'+object_id+'='+prop_name+']').each(function () {
-				var $bound = $(this);
+        var data_attr = "bind-" + object_id,
+            message = object_id + ":change";
 
-				if($bound.is("input,textarea,select")){
-					$bound.val(new_val);
-				}else{
-					$bound.html(new_val);
-				}
-			})
-		});
+        $(document).on('change', '[data-' + object_id + ']', function (event) {
+            var $input = $(this);
+            pubSub.trigger(message, [$input.data(data_attr), $input.val()]);
+        });
 
-		return pubSub;
-	}
+        pubSub.on('message', function (event, prop_name, new_val) {
+            $('[data-' + object_id + '=' + prop_name + ']').each(function () {
+                var $bound = $(this);
 
+                if ($bound.is("input,textarea,select")) {
+                    $bound.val(new_val);
+                } else {
+                    $bound.html(new_val);
+                }
+            })
+        });
 
-	// 将从learnCloud得到的notebook对象拷贝成我们需要的对象
-	util.cloneNotebook = function(fromObj,toObj) {
-		toObj  = toObj || {};
-		toObj.id = fromObj.id;
-		toObj.title = fromObj.attributes.title;
-		toObj.numberOfNote = parseInt(fromObj.attributes.numberOfNote);
-		return toObj;
-	}
-	util.findSelectOne = function(title,array){
-		var target={};
-		array.forEach(function(item){
-			if(item.title == title){
-				target = item;
-			}
-		})
-		return target;
-		console.log("something wrong as we can't find correct notebook");
-	}
-
-	util.cloneEssay    = function(fromObj,toObj) {
-		toObj = toObj || {};
-		toObj.id = fromObj.id;
-		toObj.title = fromObj.attributes.title;
-		toObj.content = fromObj.attributes.content;
-		toObj.short   = fromObj.attributes.content.replace(/\n/g,"").slice(0,80);
-		toObj.short += toObj.content.length>80 ?'...':'';
-		toObj.date    = new Date(fromObj.createdAt);
-		return toObj;
-	}
+        return pubSub;
+    }
 
 
-	window.util = util;
-})(this,this.document)
+    // 将从learnCloud得到的notebook对象拷贝成我们需要的对象
+    util.cloneNotebook = function (fromObj, toObj) {
+        toObj = toObj || {};
+        toObj.id = fromObj.id;
+        toObj.title = fromObj.attributes.title;
+        toObj.numberOfNote = parseInt(fromObj.attributes.numberOfNote);
+        return toObj;
+    }
+    util.findSelectOne = function (title, array) {
+        var target = {};
+        array.forEach(function (item) {
+            if (item.title == title) {
+                target = item;
+            }
+        })
+        return target;
+        console.log("something wrong as we can't find correct notebook");
+    }
+
+    util.cloneEssay = function (fromObj, toObj) {
+        toObj = toObj || {};
+        toObj.id = fromObj.id;
+        toObj.title = fromObj.attributes.title;
+        toObj.content = fromObj.attributes.content;
+        toObj.short = fromObj.attributes.content.replace(/\n/g, "").slice(0, 80);
+        toObj.short += toObj.content.length > 80 ? '...' : '';
+        toObj.date = new Date(fromObj.createdAt);
+        return toObj;
+    }
+
+
+    window.util = util;
+})(this, this.document)
